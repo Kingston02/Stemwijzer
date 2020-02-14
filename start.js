6384b4d377c861577ac9313f6c509584782e67ed
Kingston <kingston@vanlaak.cc>
2020-02-14 09:53:02 +0100

    commit

---
+++ start.js
@@ -0,0 +1,9 @@
+var firstContainer = document.getElementById('first-container');
+var secondContainer = document.getElementById('second-container');
+var thirdContainer = document.getElementById('third-container');
+
+
+function start(){
+    secondContainer.style.display = 'none';
+    thirdContainer.style.display = 'none';
+}
