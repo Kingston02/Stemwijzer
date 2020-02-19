ca21919592d3c47242f8998c4fb8f71ffbf544a7
Kingston <kingston@vanlaak.cc>
2020-02-19 09:05:58 +0100

    layout

--- js/start.js
+++ js/start.js
@@ -1,9 +1,15 @@
-var firstContainer = document.getElementById('first-container');
-var secondContainer = document.getElementById('second-container');
-var thirdContainer = document.getElementById('third-container');
+var firstContainer = document.getElementById("first-container");
+var secondContainer = document.getElementById("second-container");
+var thirdContainer = document.getElementById("third-container");
+var logo = document.getElementById("logo");
 
 
 function start(){
-    secondContainer.style.display = 'none';
-    thirdContainer.style.display = 'none';
+    secondContainer.style.display = "none";
+    thirdContainer.style.display = "none";
+    logo.src = "img/arrow.png";
+    logo.style.width = "20px";
+    logo.style.position = 'absolute';
+    logo.style.marginTop = '0px';
+    logo.style.marginLeft = '0px';
 }
